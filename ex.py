35d7ea7e69b964ce5bfae9b15153c1f9a2423351
Sergey Shitov <sergej.shitov.99@mail.ru>
2023-02-08 14:47:13 +0300

    + class Note

--- ex.py
+++ ex.py
@@ -1,32 +1,10 @@
-from typing import List
 from pydantic import BaseModel
+import uuid
+import datetime
 
+class Note(BaseModel):
+    id = uuid.uuid4()
+    title: str
+    text: str
+    data_time = datetime.datetime.now()
 
-class Foo(BaseModel):
-    count: int
-    size: float = None
-
-
-class Bar(BaseModel):
-    apple = 'x'
-    banana = 'y'
-
-
-class Spam(BaseModel):
-    foo: Foo
-    bars: List[Bar]
-
-m = Spam(foo={'count': 4}, bars=[{'apple': 'x1'}, {'apple': 'x2'}])
-print(m)
-#> foo=Foo(count=4, size=None) bars=[Bar(apple='x1', banana='y'),
-#> Bar(apple='x2', banana='y')]
-print(m.dict())
-"""
-{
-    'foo': {'count': 4, 'size': None},
-    'bars': [
-        {'apple': 'x1', 'banana': 'y'},
-        {'apple': 'x2', 'banana': 'y'},
-    ],
-}
-"""
