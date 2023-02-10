b7dc9f44015f85d648fb77e202a71388858c520a
Sergey Shitov <sergej.shitov.99@mail.ru>
2023-02-10 14:09:37 +0300

    + class Note

--- ex.py
+++ ex.py
@@ -1,10 +1,20 @@
-from pydantic import BaseModel
+#from pydantic import BaseModel
 import uuid
 import datetime
 
-class Note(BaseModel):
-    id = uuid.uuid4()
+class Note:
+    id: str
     title: str
     text: str
-    data_time = datetime.datetime.now()
+    data_time = str
 
+    def __init__(self, id, title, text, data_time):
+        self.id = id
+        self.title = title
+        self.text = text
+        self.data_time = data_time
+
+
+first = Note(uuid.uuid4(), input(), input(), datetime.datetime.now())
+print(first.__dict__)
+print(first.id, first.title, first.text, first.data_time)
